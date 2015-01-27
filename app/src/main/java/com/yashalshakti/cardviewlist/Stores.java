bbe7135e7d5f6efc580213da84cd71220d5f78cb
Yashal Shakti <shaktiyashal@gmail.com>
2015-01-27 23:53:26 +0530

    Initial commit

---
+++ app/src/main/java/com/yashalshakti/cardviewlist/Stores.java
@@ -0,0 +1,14 @@
+package com.yashalshakti.cardviewlist;
+
+/**
+ * Created by Yashal on 1/26/2015.
+ */
+public class Stores {
+    public String storeName;
+   public int storeImage;
+
+public Stores(String s)
+{
+    storeName=s;
+}
+}
